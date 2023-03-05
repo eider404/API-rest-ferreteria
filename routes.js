a365878b52070ea0de486c86983faaa89ff5c61a
Eider Pool <eider.pool.arjona@gmail.com>
2023-03-04 20:07:26 -0600

    endpoint post-new

--- routes.js
+++ routes.js
@@ -19,6 +19,27 @@ routes.get('/products',(req, res)=>{
 
 
 
+routes.post('/new',(req, res)=>{
+    const date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    req.body.fechaCreacion = date
+
+    if(!(req.body.nombre && req.body.precio && req.body.img)){
+        return res.status(401).json({status: 401, mensaje: "Campos obligarorios", data: req.body})
+    }
+    
+    req.getConnection((err, conn)=>{
+
+        conn.query("INSERT INTO FerrProductos set ?", [req.body], (err, rows)=>{
+            if(err) { return res.status(401).json({status: 401, mensaje: "Algo salio mal", data: req.body, err: err}) }
+            
+            return res.status(200).json({status: 200, mensaje: "Producto Agregado", data: req.body, rows: rows})
+        })
+    })
+})
+
+
+
+
 
 
 
